0f964ce2cecd0ce13256f9357bc6cc8d3bf094b3
danilbahari <danilmatahelumual@gmail.com>
2025-08-23 09:50:39 +0700

    feat: add basic text extraction with PyPDF2 (not working well)

---
+++ basic python/extract_text.py
@@ -0,0 +1,19 @@
+from PyPDF2 import PdfReader
+import sys
+
+def extract_text(input_file):
+    reader = PdfReader(input_file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("❌ Cara pakai: python extract_text.py input.pdf")
+        sys.exit(1)
+
+    input_pdf = sys.argv[1]
+    result = extract_text(input_pdf)
+
+    print(result)
